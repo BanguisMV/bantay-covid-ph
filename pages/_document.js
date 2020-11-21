108a50308a0632a5b814e096a03d6f9a1f197712
Marky <MarkVergelBanguis@gmail.com>
2020-11-20 21:08:50 -0800

    Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,15 @@ class MyDocument extends Document {
             <link rel="shortcut icon" href="/no-virus.png" />
             <link rel="apple-touch-icon" href="/no-virus.png" />
             <meta name="theme-color" content="#7579e7" />
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-WLZN988T7N"></script>
+            <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', 'G-WLZN988T7N');`
+                    }}
+          />
         </Head>
         <body>
           <Main />
